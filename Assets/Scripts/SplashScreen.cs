e3911d5c067aae43ff1a3fd0f79346a81850243d
ctmarks <cmarks@fullsail.com>
2019-03-28 10:40:08 -0400

    Added the start of a splash screen script.

--- Assets/Scripts/SplashScreen.cs
+++ Assets/Scripts/SplashScreen.cs
@@ -2,8 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// SplashScreen.cs
+/// Christopher Marks
+/// 03/28/2019
+/// </summary>
+
+
+[RequireComponent(typeof(AudioSource))]                                 // Add audio source when attaching script
 public class SplashScreen : MonoBehaviour
 {
+    public Texture2D _splashScreenBackground;                           // Creates slot in inspector for background splashscreen image
+    public Texture2D _splashScreenText;                                 // Creates slot in inspector to assign splashscreen text
+
+    private AudioSource _splashScreenAudio;                             // Defines naming convention for audio source component 
+    public AudioClip _splashScreenMusic;                                // Creates slot in inspector to assign splash screen music
+
+    private float _splashScreenFadeValue;                               // Defines fade value 
+    private float _splashScreenFadeSpeed = .15f;                        // Defines fade speed
+
+    private SplashScreenController splashScreenController;              // Defines naming convention for splash screen controller
+
+    private enum SplashScreenController                                 // Defines states for splash screen
+    {
+        SplashScreenFadeIn = 0,
+        SplashScreenFadeOut = 1
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +39,15 @@ public class SplashScreen : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void SplashScreenFadeIn()
+    {
+        Debug.Log("SplashScreenFadeIn");
+    }
+
+    private void SplashScreenFadeOut()
+    {
+        Debug.Log("SplashScreenFadeOut");
     }
 }
