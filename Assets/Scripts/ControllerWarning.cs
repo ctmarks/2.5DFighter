3962a5f67e215950ce9b9f3a11e65b483414338e
ctmarks <cmarks@fullsail.com>
2019-04-01 16:33:58 -0400

    Controller warning script: Setup fade and coroutine
    Added new scene: MainMenu

--- Assets/Scripts/ControllerWarning.cs
+++ Assets/Scripts/ControllerWarning.cs
@@ -30,6 +30,56 @@ public class ControllerWarning : ControllerManager
     // Update is called once per frame
     void Update()
     {
-        
+        if(_controllerDetected == true)                             // If controller detected equals true
+        {
+            StartCoroutine("WaitToLoadMainMenu");                   // Start WaitToLoadMainMenu function
+        }
+
+        if(_controllerConditionsMet == false)                       // If controller conditions met equals false
+            return;                                                 // then do nothing and return
+
+        if(_controllerConditionsMet == true)                        // If controller condionts met equals true
+        {
+            _controllerWarningFadeValue -=                          // Decrease fade value 
+                _controllerWarningFadeSpeed * Time.deltaTime;       // by fade speed x time.deltatime
+        }
+
+        if(_controllerWarningFadeValue < 0)                         // If fade value is less than zero
+            _controllerWarningFadeValue = 0;                        // then set fade value equal to zero
+
+        if(_controllerWarningFadeValue == 0)                        // IF fade value is equal to zero
+        {
+            _startUpFinished = true;                                // Set startup finished to true
+            SceneManager.LoadScene("MainMenu");                     // and Load the MainMenu scene
+        }
+    }
+
+    private IEnumerator WaitToLoadMainMenu()
+    {
+        yield return new WaitForSeconds(2);                         // Wait for this (x) many seconds
+
+        _controllerConditionsMet = true;                            // Set controller conditions met to true
+    }
+
+    private void OnGUI()
+    {
+        GUI.DrawTexture(new Rect                                    // Draw texture starting at 0,0
+            (0, 0, Screen.width, Screen.height),                    // by the screen width and height
+            _controllerWarningBackground);                          // draw the warningbackground
+
+        GUI.color = new Color(1, 1, 1,                              // GUI color is equal to 1,1,1(RGB default)
+            _controllerWarningFadeValue);                           // plus the fade value
+
+        GUI.DrawTexture(new Rect                                    // Draw texture starting at 0,0
+            (0, 0, Screen.width, Screen.height),                    // by the screen width and height
+            _controllerWarningText);                                // draw the warning text
+
+        if(_controllerDetected == true)                             // If controller detected equals true
+        {
+            GUI.DrawTexture(new Rect                                    // Draw texture starting at 0,0
+                (0, 0, Screen.width, Screen.height),                    // by the screen width and height
+                _controllerDetectedText);                               // draw the controller detected text
+        }
+
     }
 }
