79f865b0831753bc35f5dc20ace59815bbadaf24
ctmarks <cmarks@fullsail.com>
2019-04-02 18:05:42 -0400

    Added Texture assets for the ControllerWarning script. Made controller warning prefab and controller manager prefab

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        Cursor.visible = false;                         // Set the cursor visible to false on Awake
+        Cursor.lockState = CursorLockMode.Locked;       // and lock the cursor on awake also
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this);                        // Don't destroy this gameobject when loading a new scene
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
