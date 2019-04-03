0351413cd0cd059b9e311f98ce12c490f9e5c647
ctmarks <cmarks@fullsail.com>
2019-04-02 20:39:25 -0400

    New scene: Choose Character MainMenu script: progress of filling in switch block

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -62,8 +62,21 @@ public class MainMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        _startingOnePlayerGame = false;                                 // Starting one player game is false on startup
+        _startingTwoPlayerGame = false;                                 // Starting two player game is false on startup
+        _quittingGame = false;                                          // Qutting game set to false on startup
         _ps4Controller = false;                                         // Sets ps4 controller to false at start
         _xBOXController = false;                                        // Sets xBOX controller to false at start
+
+        _mainMenuFadeValue = 0;                                         // Main menu fade value set to zero on startup
+
+        _mainMenuAudio = GetComponent<AudioSource>();                   // _mainMenuAudioSource equals audiosource component
+
+        _mainMenuAudio.volume = 0;                                      // Audio volume equals zero on startup
+        _mainMenuAudio.clip = _mainMenuMusic;                           // Audio clip equals main menu music
+        _mainMenuAudio.loop = true;                                     // Audio source set to loop on startup
+        _mainMenuAudio.Play();                                          // Play audio
+            
 
         _mainMenuController = MainMenuController.MainMenuFadeIn;        // State equals Fade in on startup
 
@@ -99,20 +112,61 @@ public class MainMenu : MonoBehaviour
     private void MainMenuFadeIn()
     {
         Debug.Log("MainMenuFadeIn");
+
+        _mainMenuAudio.volume += _mainMenuFadeSpeed * Time.deltaTime;         // Increase volume by the fade speed
+
+        _mainMenuFadeValue += _mainMenuFadeSpeed * Time.deltaTime;            // Increase fade value by the fade speed
+
+        if (_mainMenuFadeValue > 1)                                             // If fade value is greater than 1
+            _mainMenuFadeValue = 1;                                             // Then make fade value equal to 1
+
+        if (_mainMenuFadeValue == 1)                                           // If fade value equals 1
+            _mainMenuController =                                              // then make state equal to
+                MainMenu.MainMenuController.MainMenuAtIdle;                    // main menu at idle
     }
 
     private void MainMenuAtIdle()
     {
         Debug.Log("MainMenuAtIdle");
+        if (_startingOnePlayerGame || _quittingGame == true) ;                  // If starting one player game or quitting game equals true
+        _mainMenuController = MainMenu.MainMenuController.MainMenuFadeOut;      // Set main menu controller to fade out
     }
 
     private void MainMenuFadeOut()
     {
         Debug.Log("MainMenuFadeOut");
+
+        _mainMenuAudio.volume -= _mainMenuFadeSpeed * Time.deltaTime;         // Decrease volume by the fade speed
+
+        _mainMenuFadeValue -= _mainMenuFadeSpeed * Time.deltaTime;            // Decrease fade value by the fade speed
+
+        if (_mainMenuFadeValue < 0)                                             // If fade value is less than 0
+            _mainMenuFadeValue = 0;                                             // Then make fade value equal to 0
+
+        if (_mainMenuFadeValue == 0 && _startingOnePlayerGame == true)        // If fade value equals zero AND starting one player game is equal to true
+            SceneManager.LoadScene("ChooseCharacter");                                              
     }
 
     private void MainMenuButtonPress()
     {
         Debug.Log("MainMenuButtonPress");
+
+        GUI.FocusControl(_mainMenuButtons[_selectedButton]);
+
+        switch (_selectedButton)
+        {
+            case 0:
+                _mainMenuAudio.PlayOneShot(_mainMenuStartButtonAudio);  // Play start button audio clip
+                _startingOnePlayerGame = true;                          // Set starting one player game to true
+                break;
+            case 1:
+                _mainMenuAudio.PlayOneShot(_mainMenuStartButtonAudio);  // Play start button audio clip
+                _startingTwoPlayerGame = true;                          // Set starting two player game to true
+                break;
+            case 2:
+                _mainMenuAudio.PlayOneShot(_mainMenuQuitButtonAudio);   // Play start button audio clip
+                _quittingGame = true;                                   // Set quitting game equal to true
+                break;
+        }
     }
 }
